a618876a4bde55991480e3afaa7d753da8846641
Knucle2002 <nicolas.galarraga@udla.edu.ec>
2022-09-21 22:50:41 -0500

    Banda Solicitud

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask_app.controllers import users
 from flask_app.controllers import eventos
 from flask_app.controllers import banda
 from flask_app.controllers import solicitudes
+from flask_app.controllers import solicitudes_banda
 
 
 if __name__=="__main__":
